837e85205177da11364348f84746c3739d6df549
Tõnn <tonn.toomsalu@gmail.com>
2020-01-07 13:46:25 +0200

    Add create note and delete note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateNote from "./createNote";
+
 
 function App() {
+    const [notes, setNotes] = useState([]);
+  
+    function addNote(newNote) {
+      setNotes(prevNotes => {
+        return [...prevNotes, newNote];
+      });
+    }
+  
+    function deleteNote(id) {
+      setNotes(prevNotes => {
+        return prevNotes.filter((noteItem, index) => {
+          return index !== id;
+        });
+      });
+    }
+  
     return (
-        <div>
-            <Header />
-            <Footer />
-            <Note />
-        </div>
-    )
-}
+      <div>
+        <Header />
+        <CreateNote onAdd={addNote} />
+        {notes.map((noteItem, index) => {
+          return (
+            <Note
+              key={index}
+              id={index}
+              title={noteItem.title}
+              content={noteItem.content}
+              onDelete={deleteNote}
+            />
+          );
+        })}
+        <Footer />
+      </div>
+    );
+  }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import RemoveIcon from '@material-ui/icons/Remove';
 
-function Note() {
-    return (
-        <div className="note">
-            <h1>title</h1>
-            <p>content</p>
-        </div>
-    )
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}><RemoveIcon/></button>
+    </div>
+  );
 }
 
 export default Note;
